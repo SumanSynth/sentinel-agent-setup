6c8f21a2a04602df461a3446fb89bb225cf1bc15
Suman D. <107029526+SumanSynth@users.noreply.github.com>
2024-08-23 18:58:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ echo "Installing sentinel agent"
 wget https://raw.githubusercontent.com/SumanSynth/sentinel-agent-setup/develop/sentinel-agent-linux-amd64
 sudo mkdir /opt/sentinel-agent
 sudo mv sentinel-agent-linux-amd64 /opt/sentinel-agent/sentinel-agent-linux-amd64
+sudo chmod +x /opt/sentinel-agent/sentinel-agent-linux-amd64
 
 device_id=$(uuidgen)
 echo "device_id: $device_id"
