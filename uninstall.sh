a5d34a2c0d970f0f711d43bf6f89b2ef084171e1
Suman D. <107029526+SumanSynth@users.noreply.github.com>
2024-08-25 09:54:54 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,12 @@
 
 echo "Un installing sentinel agent"
 
+# Check if the directory doesn't exist
+if [ ! -d "/opt/sentinel-agent" ]; then
+  echo "Directory /opt/sentinel-agent doesn't exist."
+  exit 1
+fi
+
 echo "Disabling the sentinel service to start on boot"
 sudo systemctl disable sentinel-agent.service
 
