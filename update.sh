2f73bf91d082e5c2b671d614f2452e3db4fa6456
Suman D. <107029526+SumanSynth@users.noreply.github.com>
2024-09-12 15:45:03 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,8 +7,20 @@ if [ ! -d "/opt/sentinel-agent" ]; then
   exit 1
 fi
 
-# Download updated agent
-wget https://github.com/SumanSynth/sentinel-agent-setup/releases/download/v1.0.1/sentinel-agent-linux-amd64
+# Check CPU architecture
+arch=$(uname -m)
+
+if [[ "$arch" == "x86_64" ]]; then
+  echo "AMD architecture detected."
+  wget https://github.com/SumanSynth/sentinel-agent-setup/releases/download/v1.0.1/sentinel-agent-linux-amd64
+elif [[ "$arch" == "arm64" || "$arch" == "aarch64" ]]; then
+  echo "ARM architecture detected."
+  wget https://github.com/SumanSynth/sentinel-agent-setup/releases/download/v1.0.1/sentinel-agent-apple-arm64
+  sudo mv sentinel-agent-linux-amd64 sentinel-agent-linux-amd64
+else
+  echo "Unknown architecture: $arch"
+  exit 1
+fi
 
 sudo mv sentinel-agent-linux-amd64 /opt/sentinel-agent/
 sudo chmod +x /opt/sentinel-agent/sentinel-agent-linux-amd64
